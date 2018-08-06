bec99713b407fb9dbfad88b43650159860dddcd3
AlexinaO <alexina.orsoni@gmail.com>
2018-08-06 10:47:10 +0200

    enum

--- Exos_2018.08.06/Program.cs
+++ Exos_2018.08.06/Program.cs
@@ -10,6 +10,25 @@ namespace Exos_2018._08._06
     {
         static void Main(string[] args)
         {
+            var evenement = new Evenement();
+            evenement.JourSemaine = JourSemaine.Lundi;
+
+            Console.ReadKey();
+
+
+        }
+        public class Evenement
+        { public JourSemaine JourSemaine { get; set; }
+        }
+        public enum JourSemaine
+        {
+            Lundi = 1,
+            Mardi = 2,
+            Mercredi = 3,
+            Jeudi = 4,
+            Vendredi = 5,
+            Samedi = 6,
+            Dimanche = 7
         }
     }
 }
